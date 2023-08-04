d263a53be0d505f0f4902ced85276bc10710ad4f
hae yeong <hys03140314@gmail.com>
2023-08-04 15:10:38 +0900

    Merge branch 'main' of https://github.com/hyshys0314/javaPro.git into main

--- javaPro/src/sample/cutOperation.java
+++ javaPro/src/sample/cutOperation.java
@@ -4,11 +4,9 @@ public class cutOperation {
 
 	public static void main(String[] args) {
 		
-		//홍길동이 와서 코딩을 완벽하게 해놓음
-		
 		System.out.println("hello world");
 
-			//강사님이 코딩하심
+			//강사님꺼만 코딩하심
 	}
 
 }
